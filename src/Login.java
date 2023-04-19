cf2091d38a7de38baf1283881ec7061a0de400f4
LuisOrg26 <luisjaorg26mail.com>
2023-04-18 19:46:16 -0600

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+
+public class Login {
+
+    private JPanel panel1;
+    //create a label that can me moved freely
+    
+    private JLabel lbluser;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
